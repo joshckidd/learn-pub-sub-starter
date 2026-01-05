025214619750af27a1ef74aa45c0924caf1750cd
joshckidd <josh@larameekidd.com>
2026-01-05 10:06:30 -0500

    initial commit

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
+	const connectionString string = "amqp://guest:guest@localhost:5672/"
 	fmt.Println("Starting Peril server...")
+
+	con, _ := amqp.Dial(connectionString)
+	defer con.Close()
+
+	fmt.Println("Connection successful!")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
+
+	fmt.Println("Stopping Peril server...")
 }
